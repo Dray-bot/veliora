ff67df10560f161e810b66450fccd1711b1821e2
Ayomide <ibiyemiayomide180@gmail.com>
2025-08-09 00:10:38 +0100

    fix: Add 'thumbs.dreamstime.com' to allowed image domains

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const nextConfig = {
             'www.framer.com',
             'media.istockphoto.com',
             'www.freepik.com',
+            'thumbs.dreamstime.com'
         ],
     },
 };
